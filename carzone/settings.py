79b8f4a8ebbbde7b4bb790ec05d1f68a9c39f2b9
Mazed <raiyoz@live.com>
2025-01-31 19:51:24 +0700

    commit

--- carzone/settings.py
+++ carzone/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-()ikt3skmt&pknr+k$hj(y678x8qeuvln)utyuqc!fmx8e1o=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
