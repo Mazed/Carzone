3aa4cb888b189635f8aaaf1d9b851ee2b2543141
Mazed <raiyoz@live.com>
2025-01-31 19:18:21 +0700

    commit

--- carzone/settings.py
+++ carzone/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-()ikt3skmt&pknr+k$hj(y678x8qeuvln)utyuqc!fmx8e1o=s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
